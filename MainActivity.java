1605762a37d76f75b3dee303e5098c8132239193
kyungjung kim <kj.kim@live.co.kr>
2019-03-18 17:04:13 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,78 @@
+package kr.co.prosoft.slidingview;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.graphics.Color;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private final String[] navItems = {"1", "2", "3", "4", "5"};
+    private ListView lvNavList;
+    private FrameLayout flContainer;
+    private DrawerLayout dlDrawerLayout;
+    private Button btn;
+
+
+    @Override
+    public void onBackPressed() {
+        if (dlDrawerLayout.isDrawerOpen(lvNavList)) {
+            dlDrawerLayout.closeDrawer(lvNavList);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        lvNavList = (ListView) findViewById(R.id.lv_activity_main_nav_list);
+        flContainer = (FrameLayout) findViewById(R.id.fl_activity_main_container);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dlDrawerLayout.openDrawer(lvNavList);
+            }
+        });
+
+        dlDrawerLayout = (DrawerLayout) findViewById(R.id.dl_activity_main_drawerlayout);
+        lvNavList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navItems));
+        lvNavList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+            switch (position) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+
+            dlDrawerLayout.closeDrawer(lvNavList);
+        }
+    }
+}
+
