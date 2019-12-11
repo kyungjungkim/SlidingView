06c8ed3b81f947b38dd33df48384df599060550e
kyungjung kim <kj.kim@live.co.kr>
2019-12-11 23:18:58 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -14,7 +14,8 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -195,5 +196,26 @@ public class MainActivity extends AppCompatActivity {
 //                dlDrawerLayout.closeDrawer(lvNavList);
             }
         });
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
+        tabHost1.setup();
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("TAB 1");
+        tabHost1.addTab(ts1);
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
+        ts2.setContent(R.id.content2);
+        ts2.setIndicator("TAB 2");
+        tabHost1.addTab(ts2);
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
+        ts3.setContent(R.id.content3);
+        ts3.setIndicator("TAB 3");
+        tabHost1.addTab(ts3);
     }
 }
